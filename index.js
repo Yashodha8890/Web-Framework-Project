5aff25b8b95ac8d5274e06191d4f209548029804
Shammi Tharanga Prabani Premarathna <113094074+shammi22000@users.noreply.github.com>
2025-04-29 13:10:08 +0300

    added expense tracker changes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const viewExpense = require('./models/expenseView');
+const Expense = require('./models/expense');
 
 //const { default: mongoose } = require('mongoose');
 const app = express();
@@ -41,6 +43,86 @@ app.get('/activityTracker',(req,res) => {
         title: "Add New Activity"
     });
 });
+
+//expense.handlebars
+app.get('/addExpense',(req,res) => {
+    res.render('addExpense',{
+        title: "Add New expense"
+    });
+});
+
+//add expense date to database
+app.post('/addExpense', async(req,res) => {
+    try
+    {
+        const newExpenses = new Expense(req.body)
+        await newExpenses.save();
+        res.send('added expense!!');
+    }
+
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).send('Error expense add!!')
+    }
+    
+})
+//get all expenses from MongoDB and print to the webpage
+app.get('/viewExpense', async (req, res) => {
+    try {
+      const expenses  = await Expense.find().lean();
+      res.render('viewExpense', 
+        {
+        title: 'Daily Expenses',
+        expenses: expenses
+        }
+    )
+    } 
+    catch (error) 
+        {
+            console.error('Error fetching expenses:', error);
+            res.status(500).send('Internal Server Error');
+        }
+  });
+
+  // get Update expense data
+app.get('/updateExpense/:id', async (req, res) => {
+    try {
+      const expense = await Expense.findById(req.params.id).lean();
+      if (!expense) return res.status(404).send('Expense not found');
+      res.render('updateExpense', { expense });
+    } catch (err) {
+      res.status(500).send('Server Error');
+    }
+  });
+
+  //  update the expenses in view page
+app.post('/updateExpense/:id', async (req, res) => {
+    try {
+      await Expense.findByIdAndUpdate(req.params.id, {
+        amount: req.body.amount,
+        category: req.body.category,
+        paymentMethod: req.body.paymentMethod,
+        date: req.body.date,
+        notes: req.body.notes
+      });
+      res.redirect('/viewExpense');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Failed to update expense');
+    }
+  });
+
+  // DELETE Expenses by ID 
+app.post('/deleteExpense/:id', async (req, res) => {
+    try {
+      await Expense.findByIdAndDelete(req.params.id).lean();
+      res.redirect('/viewExpense'); 
+    } catch (error) {
+      console.error('Error deleting expense:', error);
+      res.status(500).send('Failed to delete expense');
+    }
+  });
 
 //This is the folder which contains files like css, imgs
 app.use(express.static('public'));
